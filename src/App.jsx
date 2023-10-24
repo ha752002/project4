939bec0e40ae7d5effd95dd7897331aff58427bd
ha752002 <honggha75@gmail.com>
2023-10-24 12:14:08 +0700

    theming

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 // eslint-disable-next-line semi
 import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 function App() {
   return (
     <>
+      <div>Hong Ha</div>
+      <Typography variant="body2" color="text.secondary">
+        Test Typography
+      </Typography>
+
       <Button variant="contained">Hello world</Button>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-/* eslint-disable semi */
-/* eslint-disable quotes */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CssBaseline></CssBaseline>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );

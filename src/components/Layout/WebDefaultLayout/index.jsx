0c11f174e7e0d35649faeb62104e5ee09114cad1
ha752002 <honggha75@gmail.com>
2023-10-31 20:08:22 +0700

    fix function cpn

--- src/components/Layout/WebDefaultLayout/index.jsx
+++ src/components/Layout/WebDefaultLayout/index.jsx
@@ -1,36 +1,15 @@
 import classNames from "classnames/bind";
-
 // import "./DefaultLayout.module.scss";
-import Header from "../WebComponents/Header";
 
-// const cx = classNames.bind(styles);
-
-// function WebDefaultLayout({ children }) {
-//   return (
-//     <div>
-//       <Header />
-//       <div>
-//         <div>{children}</div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default WebDefaultLayout;
-import React, { Component } from "react";
-import clsx from "clsx";
-
-export default class WebDefaultLayout extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <>
-        {" "}
-        <div>{this.props.children}</div>
-      </>
-    );
-  }
+function WebDefaultLayout({ children }) {
+  return (
+    <div>
+      {/* <Header /> */}
+      <div>
+        <div>{children}</div>
+      </div>
+    </div>
+  );
 }
+
+export default WebDefaultLayout;

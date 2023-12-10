824f8c42053ed874f88151a8657f1c863a688eb0
ha752002 <honggha75@gmail.com>
2023-12-10 20:34:29 +0700

    update

--- src/admin/ProductsManagement/AddProductManagement.jsx
+++ src/admin/ProductsManagement/AddProductManagement.jsx
@@ -21,72 +21,99 @@ const AddProductManagement = () => {
     }));
   };
 
-  const handleEditTodo = (index) => {
-    setEditIndex(index);
-    setEditedTodo(todos[index]);
+  const handleSpecificationChange = (index, e) => {
+    const { name, value } = e.target;
+
+    setProductData((prevData) => {
+      const updatedSpecifications = [...prevData.specifications];
+      updatedSpecifications[index] = {
+        ...updatedSpecifications[index],
+        [name]: value,
+      };
+
+      return {
+        ...prevData,
+        specifications: updatedSpecifications,
+      };
+    });
   };
 
-  const handleSaveEdit = () => {
-    const newTodos = [...todos];
-    newTodos[editIndex] = editedTodo;
-    setTodos(newTodos);
-    setEditIndex(null);
-    setEditedTodo("");
+  const handleCategoryChange = (e) => {
+    const { name, value } = e.target;
+
+    setProductData((prevData) => ({
+      ...prevData,
+      categories: [{ id: value }],
+    }));
   };
 
-  const handleDeleteTodo = (index) => {
-    const newTodos = todos.filter((_, i) => i !== index);
-    setTodos(newTodos);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Đưa dữ liệu tạo sản phẩm về server hoặc thực hiện các tác vụ cần thiết
+    console.log("Submitted Product Data:", productData);
   };
 
   return (
-    <div className="container mt-5">
-      <form onSubmit={handleSubmit}>
-        {/* Các trường nhập liệu cho sản phẩm */}
-        <div>
-          <label>Title:</label>
-          <input
-            type="text"
-            name="title"
-            value={productData.title}
-            onChange={handleChange}
-          />
-        </div>
-
-        <div>
-          <label>Product Code:</label>
-          <input
-            type="text"
-            name="productCode"
-            value={productData.productCode}
-            onChange={handleChange}
-          />
-        </div>
-
-        {/* ...Thêm các trường khác tương tự */}
-
-        {/* Trường nhập liệu cho Specifications */}
-        {productData.specifications.map((specification, index) => (
-          <div key={index}>
-            <label>{`Specification ${index + 1} Name:`}</label>
+      <div className="container mt-5">
+        <form onSubmit={handleSubmit}>
+          {/* Các trường nhập liệu cho sản phẩm */}
+          <div>
+            <label>Title:</label>
             <input
-              type="text"
-              name={`specifications[${index}].name`}
-              value={specification.name}
-              onChange={(e) => handleSpecificationChange(index, e)}
-            />
-
-            <label>{`Specification ${index + 1} Content:`}</label>
-            <input
-              type="text"
-              name={`specifications[${index}].content`}
-              value={specification.content}
-              onChange={(e) => handleSpecificationChange(index, e)}
+                type="text"
+                name="title"
+                value={productData.title}
+                onChange={handleChange}
             />
           </div>
-        ))}
-      </ul>
-    </div>
+
+          <div>
+            <label>Product Code:</label>
+            <input
+                type="text"
+                name="productCode"
+                value={productData.productCode}
+                onChange={handleChange}
+            />
+          </div>
+
+          {/* ...Thêm các trường khác tương tự */}
+
+          {/* Trường nhập liệu cho Specifications */}
+          {productData.specifications.map((specification, index) => (
+              <div key={index}>
+                <label>{`Specification ${index + 1} Name:`}</label>
+                <input
+                    type="text"
+                    name={`specifications[${index}].name`}
+                    value={specification.name}
+                    onChange={(e) => handleSpecificationChange(index, e)}
+                />
+
+                <label>{`Specification ${index + 1} Content:`}</label>
+                <input
+                    type="text"
+                    name={`specifications[${index}].content`}
+                    value={specification.content}
+                    onChange={(e) => handleSpecificationChange(index, e)}
+                />
+              </div>
+          ))}
+
+          {/* Trường nhập liệu cho Categories */}
+          <div>
+            <label>Category:</label>
+            <input
+                type="text"
+                name="category"
+                value={productData.categories[0].id}
+                onChange={handleCategoryChange}
+            />
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
   );
 };
 
